3ecb05f66f6d75b89d2bfa58df35ef30eb715d03
sharondsza26 <sharondsouza26@gmail.com>
2021-09-21 02:11:00 +0530

    changes

--- websd.js
+++ websd.js
@@ -11,22 +11,22 @@ var currentScrollPos = window.pageYOffset;
 
 function changeImageAndText1(){
   
-  document.getElementById("projDescript").innerHTML="Java-Mini Music Player : This simulation of a music player displays graphics simultaneously with the sound of music. Makes use of conepts in AWT, Swing, Sound, EventListener, MidiEvent, sequencer, etc.";
-  img = document.getElementById("projPic").src="proj1.PNG";
-  img.style.transform = scale(1);
+  document.getElementById("projDescript").innerHTML="MachineLearning-FoodDeliveryTimePrediction : Using machine learning concepts I developed a time prediction system along with GoogleAPI for better accuracy. It also contains a pickle model.";
+  document.getElementById("projPic").src="project1.PNG";
 
 }
 
 function changeImageAndText2(){
   
-  document.getElementById("projDescript").innerHTML="Python-Vartalap : Along with the basic concepts of a concurrent server, I built a Message Server that uses a Queue for accepting Client messages and also uses Daemon Threads. It makes use of SEDA Arcitechture.";
-  document.getElementById("projPic").src="proj2.PNG";
+  document.getElementById("projDescript").innerHTML="Java-Mini Music Player : This simulation of a music player displays graphics simultaneously with the sound of music. Makes use of conepts in AWT, Swing, Sound, EventListener, MidiEvent, sequencer, etc.";
+  img = document.getElementById("projPic").src="project2.PNG";
+  img.style.transform = scale(1);
 
 }
 
 function changeImageAndText3(){
   
-  document.getElementById("projDescript").innerHTML="MachineLearning-FoodDeliveryTimePrediction : Using machine learning concepts I developed a time prediction system along with GoogleAPI for better accuracy. It also contains a pickle model.";
-  document.getElementById("projPic").src="proj3.PNG";
+  document.getElementById("projDescript").innerHTML="Python-Vartalap : Along with the basic concepts of a concurrent server, I built a Message Server that uses a Queue for accepting Client messages and also uses Daemon Threads. It makes use of SEDA Arcitechture.";
+  document.getElementById("projPic").src="project3.PNG";
 
 }
