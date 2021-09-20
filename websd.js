152ecce5bafb880eea75b7f5873d9dacb9054d92
sharondsza26 <sharondsouza26@gmail.com>
2021-09-21 00:15:41 +0530

    renaming images

--- websd.js
+++ websd.js
@@ -12,7 +12,7 @@ var currentScrollPos = window.pageYOffset;
 function changeImageAndText1(){
   
   document.getElementById("projDescript").innerHTML="Java-Mini Music Player : This simulation of a music player displays graphics simultaneously with the sound of music. Makes use of conepts in AWT, Swing, Sound, EventListener, MidiEvent, sequencer, etc.";
-  img = document.getElementById("projPic").src="proj1.png";
+  img = document.getElementById("projPic").src="proj1.PNG";
   img.style.transform = scale(1);
 
 }
@@ -20,13 +20,13 @@ function changeImageAndText1(){
 function changeImageAndText2(){
   
   document.getElementById("projDescript").innerHTML="Python-Vartalap : Along with the basic concepts of a concurrent server, I built a Message Server that uses a Queue for accepting Client messages and also uses Daemon Threads. It makes use of SEDA Arcitechture.";
-  document.getElementById("projPic").src="proj2.png";
+  document.getElementById("projPic").src="proj2.PNG";
 
 }
 
 function changeImageAndText3(){
   
   document.getElementById("projDescript").innerHTML="MachineLearning-FoodDeliveryTimePrediction : Using machine learning concepts I developed a time prediction system along with GoogleAPI for better accuracy. It also contains a pickle model.";
-  document.getElementById("projPic").src="proj3.png";
+  document.getElementById("projPic").src="proj3.PNG";
 
 }
